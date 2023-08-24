becd81d652f787875f14ee86cb024f95d4c1579d
isurudavind <143091085+isurudavind@users.noreply.github.com>
2023-08-24 20:55:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94774515563', 'me', true],
+  ['94774515563', 'DTS DJ BOY', true], 
   [''] 
 ] //Number of owners
 
